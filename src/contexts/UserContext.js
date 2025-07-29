40ebf8460e279a53abc8d0562048737365a17b39
kev7n11f <kev7n11f@gmail.com>
2025-07-29 21:58:30 +0000

    fix: ensure useUser throws an error if used outside of UserProvider

--- src/contexts/UserContext.js
+++ src/contexts/UserContext.js
@@ -5,9 +5,10 @@ const UserContext = createContext();
 export const useUser = () => {
   const context = useContext(UserContext);
   if (!context) {
-
+    throw new Error('useUser must be used within a UserProvider');
+  }
   return context;
-
+};
 
 export const UserProvider = ({ children }) => {
   const [user, setUser] = useState(null);
